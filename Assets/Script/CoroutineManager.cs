c7ac25f022adb6d02560c2055329aa8454f9305d
bobby <shibinwork2015@gamil.com>
2016-01-15 22:44:29 +0800

    Unity Coroutine Frame First commit

---
+++ Assets/Script/CoroutineManager.cs
@@ -0,0 +1,74 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+    public class CoroutineManager
+    {
+        private static CoroutineManager instance;
+
+        public static CoroutineManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new CoroutineManager();
+                return CoroutineManager.instance;
+            }
+        }
+
+        private readonly List<CoroutineTask> activeTaskList = new List<CoroutineTask>();
+        private CoroutineManager() { }
+
+
+        public void Tick()
+        {
+            if (activeTaskList.Count > 0)
+            {
+                bool anyTaskFinish = false;
+                for (int i = 0; i < activeTaskList.Count; i++)
+                {
+                    CoroutineTask tempTask = activeTaskList[i];
+                    if (!tempTask.routine.MoveNext())
+                    {
+                        anyTaskFinish = true;
+                        tempTask.isFinish = true;
+                        activeTaskList[i] = tempTask;
+                    }
+                }
+                if (anyTaskFinish)
+                {
+                    activeTaskList.RemoveAll(x => x.isFinish == true);
+                }
+            }
+        }
+
+        public CoroutineTask StartCoroutine(IEnumerator routine)
+        {
+            if (routine.MoveNext())
+            {
+                CoroutineTask tempTask = new CoroutineTask(routine);
+                activeTaskList.Add(tempTask);
+                return tempTask;
+            }
+            return new CoroutineTask();
+        }
+
+        public void StopCoroutine(CoroutineTask routineTask)
+        {
+            if (activeTaskList.Contains(routineTask))
+                activeTaskList.Remove(routineTask);
+        }
+
+    }
+
+    public struct CoroutineTask
+    {
+        public IEnumerator routine;
+        public bool isFinish;
+
+        public CoroutineTask(IEnumerator routine)
+        {
+            this.routine = routine;
+            isFinish = false;
+        }
+    }
---
+++ Assets/Script/TestCoroutine.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TestCoroutine : MonoBehaviour
+{
+    private int num = 0;
+    private CoroutineTask curCoroutineTask;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Q))
+	    {
+	        num++;
+	        print("Start Coroutine " + num + " : ");
+            curCoroutineTask = CoroutineManager.Instance.StartCoroutine(Test());
+	    }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("Stop Coroutine " + num + " : ");
+            CoroutineManager.Instance.StopCoroutine(curCoroutineTask);
+        }
+	}
+
+    IEnumerator Test()
+    {
+        int temp = num;
+        while (true)
+        {
+            print(temp);
+            yield return null;
+        }
+    }
+}
